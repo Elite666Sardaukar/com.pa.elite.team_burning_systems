429438af61c9ea21a3b4e4d7d43d79c9829c0d01
Elite666Sardaukar <elite666sardaukar@gmail.com>
2015-02-01 16:38:06 +1000

    Team Burning Systems

---
+++ ui/mods/Team_Burning_Systems/maplist.js
@@ -0,0 +1,8 @@
+cShareSystems.load_pas("Team Burning Systems", [
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]canopus_system.pas",
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]delta_pavonis system.pas",
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]grumman.pas",
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]halo.pas",
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]lunimos.pas",
+	"coui://ui/mods/Team_Burning_Systems/systems/[tbs]the_t3mple v2.pas",
+]);
